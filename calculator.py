21b27ad40075374d53be9d08249a2f11bcd1b7e7
Batgirl33 <98470680+Batgirl33@users.noreply.github.com>
2024-04-25 01:56:23 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,31 @@
+#functions in python 
+def info(name,major,id):
+    print("Student name:"+name)
+    print("Student major:"+major)
+    print("Student id:"+str(id))
+
+info("batool","cis",577787)
+#calculator
+def sub(num1,num2):
+    return num1-num2
+   
+def sum(num1,num2):
+    return num1+num2
+
+def mult(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
+
+
+
+print(sub(8,3) )  
+print(sum(4,2))
+print(divide(25,5))
+print(mult(4,3))
+
+
+
+
+    
